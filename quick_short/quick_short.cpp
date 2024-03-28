f44c81def0d54ebec3c855f7f1414c8d1d1e73fd
gagatpadi <padiyogaradhana@gmail.com>
2024-03-28 16:33:59 +0700

    void input

--- quick_short/quick_short.cpp
+++ quick_short/quick_short.cpp
@@ -5,3 +5,20 @@ int arr[20];
 int cmp_count = 0;
 int mov_count = 0;
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan Panjang Elment Array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+	
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "n--------------------" << endl;
+}
